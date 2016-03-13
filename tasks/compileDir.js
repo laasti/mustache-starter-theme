4d4149f972756e27ef99b2d19d9f291bbe268025
nebulousGirl <contact@nebulousweb.com>
2016-03-13 18:30:22 -0400

    new compileDir arg for gulpfile

--- tasks/compileDir.js
+++ tasks/compileDir.js
@@ -1,7 +1,9 @@
 module.exports = function (gulp, plugins, opts) {
     return function () {
         var args = plugins.yargs.argv;
-        
+        if (args.compileDir) {
+			return args.compileDir;
+		} 
         for (var i in opts.compileDirs) {
             if (args[i]) {
                 return opts.compileDirs[i];
